0d47cc4b6997fa2cdb0e88ba725e2635cdbf12e4
Javlonbax <javlonsaipov@gmail.com>
2025-08-14 21:52:08 +0500

    Update dars.py

--- lesson-5/dars.py
+++ lesson-5/dars.py
@@ -16,3 +16,15 @@ sorted(countr,key=len)                  ##### so`zzi uzunligiga qarab sortirovka
 #1 - dictionary va set da ishlatamiz
 #2 - str formatda f"str{v} gdfh"
 
+ls=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in ls:
+    if i%2==0:
+        print(i,end=' ')
+
+num=int(input('raqam kirit'))
+for i in range(2,num):
+    if num%i==0:
+        print('tubsonmas')
+        break
+else:                           ### bu for loop ni else, agar for loop muammosiz ohirgacha ishlap tugasa, keyin else ishliydi
+    print('tubson')
