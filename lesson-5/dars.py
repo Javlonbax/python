f51d1cb1e8e2043918aeae0ea0811a3bfa918971
Javlonbax <javlonsaipov@gmail.com>
2025-08-14 20:24:20 +0500

    Update dars.py

--- lesson-5/dars.py
+++ lesson-5/dars.py
@@ -1,1 +1,18 @@
+countr=['az6s','uzsssss','rus']
+sorted(countr,key=len)                  ##### so`zzi uzunligiga qarab sortirovka qiladi
+
+### -  ()
+#1 - oddiy qavslar tuple da ishlatiladi
+#2 - funksiyalar bilan print(), id(), print
+#3 - metodlaar bilan ishlatiladi upper(), append(), split()
+
+#  - []
+#1 - list da ishlatiladi
+#2 - indexing da a[1],
+#3 - slicingsa ls[1:2:3]
+#4 - list comprehention, yani listni ichida for loop ishlatganda
+
+# - {}
+#1 - dictionary va set da ishlatamiz
+#2 - str formatda f"str{v} gdfh"
 
