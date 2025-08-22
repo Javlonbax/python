4cacad412ae3bb67be85b34bb76a9e8cc690faaa
Javlonbax <javlonsaipov@gmail.com>
2025-08-22 17:39:57 +0500

    Update home.py

--- lesson-8/homework/home.py
+++ lesson-8/homework/home.py
@@ -1,1 +1,19 @@
+# Exception Handling Exercises
+# 1 Write a Python program to handle a ZeroDivisionError exception when dividing a number by zero.
+def calc(n):
+  try:
+    return 1/n
+  except ZeroDivisionError:
+    return 'Noldan boshqa son kirit!'
+print(calc(2))
 
+# 2. Write a Python program that prompts the user to input an integer and raises a ValueError exception if the input is not a valid integer
+def calc(n):
+  try:
+    return 1/int(n)
+  except ValueError:
+    return 'Raqam kirit!'
+
+print(calc(input('Son kiriting '))) 
+
+# 3 Write a Python program that opens a file and handles a FileNotFoundError exception if the file does not exist.
